1a10a2057dd322f0fcdf5119818bd8790f915e7c
Elizabeth Kok <elizabethkok_97@hotmail.com>
2022-08-27 19:17:45 +0900

    Building neural network using PyTorch

---
+++ NeuralNetworkPyTorch/torchnn.py
@@ -0,0 +1,64 @@
+import torch
+from PIL import Image
+from torch import nn, save, load #neural network class
+from torch.optim import Adam #optimizer
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor #convert images to tensors
+
+#Get data
+train=datasets.MNIST(root="data", download=True, train=True, transform=ToTensor())
+dataset=DataLoader(train, 32)
+
+#Create class
+class ImageClassifier(nn.Module):
+    def __init__(self):
+        super().__init__() #subclass the model
+        self.model=nn.Sequential(
+            nn.Conv2d(1,32,(3,3)),
+            nn.ReLU(),
+            nn.Conv2d(32,64,(3,3)), #input channels becomes 32 , output 64 channels
+            nn.ReLU(),
+            nn.Conv2d(64,64,(3,3)), #input channels becomes 64 , output 64 channels
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(64*(28-6)*(28-6),10)
+        )
+
+    def forward(self,x):
+        return self.model(x)
+
+#Instance of the neural network, loss, optimizer
+clf=ImageClassifier().to("cpu")
+opt=Adam(clf.parameters(),lr=1e-3)
+loss_fn=nn.CrossEntropyLoss()
+
+# Training flow
+if __name__=="__main__":
+    """ # uncomment to train
+    for epoch in range(10):
+        for batch in dataset:
+            X,y=batch
+            X,y=X.to("cpu"),y.to("cpu")
+            yhat=clf(X)
+            loss=loss_fn(yhat,y) #calculate our loss
+
+            #Apply backprop
+            opt.zero_grad()
+            loss.backward() #calc gradient
+            opt.step() #take a step and apply gradient descend
+
+        print(f"Epoch {epoch} Loss is {loss.item()}")
+
+    with open("model_state.pt","wb")as f:
+        save(clf.state_dict(),f)
+
+    """
+
+    with open("model_state.pt","rb") as f:
+        clf.load_state_dict(load(f))
+
+    img=Image.open("img_2.jpg")
+    img_tensor=ToTensor()(img).unsqueeze(0).to("cpu")
+
+    print(torch.argmax(clf(img_tensor)))
